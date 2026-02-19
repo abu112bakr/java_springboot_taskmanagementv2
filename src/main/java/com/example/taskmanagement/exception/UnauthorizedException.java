3e071374e2c4ba65bfb49c97eb326539f74e41a4
cod_mac <cod_mac@MacBook-Air-2.local>
2026-02-19 14:49:38 +0600

    message

--- src/main/java/com/example/taskmanagement/exception/UnauthorizedException.java
+++ src/main/java/com/example/taskmanagement/exception/UnauthorizedException.java
@@ -2,6 +2,7 @@ package com.example.taskmanagement.exception;
 
 public class UnauthorizedException extends RuntimeException{
     public UnauthorizedException(String message) {
+        
         super(message);
         System.out.println("Exception being handeled inside  UnauthorizedException: " + message);
     }
